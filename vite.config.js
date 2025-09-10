ebe0aa4176b09d75084d25545653824ee5b241be
Rachasumanth <sumanthracha81@gmail.com>
2025-09-10 12:23:41 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/', // REQUIRED: ensures asset paths work for GitHub Pages with a custom domain
+});
